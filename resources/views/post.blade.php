a5303e914cafb3619cd882a07376a9a878aa0c02
folza <fodorzoltan317@gmail.com>
2023-12-02 15:56:39 +0100

    8.Store_Blog_Post_As_HTML

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,12 +5,7 @@
 
 <body>
 <article>
-    <h1><a href="/post">My First Post</a></h1>
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A beatae delectus dolorem, dolores enim facilis illo
-        illum ipsa,
-        maxime officiis provident quam sed soluta unde vitae? Doloribus, excepturi, sequi. Exercitationem.
-    </p>
+<?= $post; ?>
 </article>
 <a href="/">Go Back</a>
 </body>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,7 +5,7 @@
 
 <body>
 <article>
-    <h1><a href="/post">My First Post</a></h1>
+    <h1><a href="/posts/my-first-post">My First Post</a></h1>
 
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A beatae delectus dolorem, dolores enim facilis illo
         illum ipsa,
@@ -13,7 +13,7 @@
     </p>
 </article>
 <article>
-    <h1><a href="/post">My Second Post</a></h1>
+    <h1><a href="/posts/my-second-post">My Second Post</a></h1>
 
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A beatae delectus dolorem, dolores enim facilis illo
         illum ipsa,
@@ -21,7 +21,7 @@
     </p>
 </article>
 <article>
-    <h1><a href="/post">My Third Post</a></h1>
+    <h1><a href="/posts/my-third-post">My Third Post</a></h1>
 
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A beatae delectus dolorem, dolores enim facilis illo
         illum ipsa,

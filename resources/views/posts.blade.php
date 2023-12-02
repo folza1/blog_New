bc2805dc241f6ef692c6034e8445f22710f53210
folza <fodorzoltan317@gmail.com>
2023-12-02 19:14:32 +0100

    11.Use_The_Filesystem_Class_To_Read_A_Directory

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,28 +4,9 @@
 <link rel="stylesheet" href="/css/app.css">
 
 <body>
+<?php foreach ($posts as $post): ?>
 <article>
-    <h1><a href="/posts/my-first-post">My First Post</a></h1>
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A beatae delectus dolorem, dolores enim facilis illo
-        illum ipsa,
-        maxime officiis provident quam sed soluta unde vitae? Doloribus, excepturi, sequi. Exercitationem.
-    </p>
+        <?= $post; ?>
 </article>
-<article>
-    <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A beatae delectus dolorem, dolores enim facilis illo
-        illum ipsa,
-        maxime officiis provident quam sed soluta unde vitae? Doloribus, excepturi, sequi. Exercitationem.
-    </p>
-</article>
-<article>
-    <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A beatae delectus dolorem, dolores enim facilis illo
-        illum ipsa,
-        maxime officiis provident quam sed soluta unde vitae? Doloribus, excepturi, sequi. Exercitationem.
-    </p>
-</article>
+<?php endforeach; ?>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,18 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
+    return view('posts',[
+        'posts'=>Post::all()
+    ]);
 });
 
 Route::get('/posts/{post}', function ($slug) {
 
-    if (!file_exists($path=__DIR__ . "/../resources/posts/{$slug}.html")){
-        return redirect('/');
-    }
+    return view('post', [
+        'post' => Post::find($slug)
+    ]);
 
-    $post = cache()->remember("posts.{$slug}",1200, function () use ($path) {
-        return file_get_contents($path);
-    });
-
-    return view('post', ['post'=>$post]);
 })->where('post', '[A-z-/_]+');

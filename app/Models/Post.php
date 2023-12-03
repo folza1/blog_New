6ad15731a0f132b0b1aa537e6d6d71b698542d96
folza <fodorzoltan317@gmail.com>
2023-12-03 17:26:19 +0100

    13.Collection_Sorting_And_Caching_Refresher

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -26,19 +26,21 @@ class Post
     }
     public static function all()
     {
-        return collect(File::files(resource_path("posts")))
-            ->map(function ($file){
-                return YamlFrontMatter::parseFile($file);
-            })
-            ->map(function($document) {
-                return new Post(
-                    $document->title,
-                    $document->excerpt,
-                    $document->date,
-                    $document->body(),
-                    $document->slug,
-                );
-            });
+        return cache()->rememberForever('posts.all',function(){
+            return collect(File::files(resource_path("posts")))
+                ->map(function ($file){
+                    return YamlFrontMatter::parseFile($file);
+                })
+                ->map(function($document) {
+                    return new Post(
+                        $document->title,
+                        $document->excerpt,
+                        $document->date,
+                        $document->body(),
+                        $document->slug,
+                    );
+                })->sortByDesc('date');
+        });
     }
 
     public static function find($slug)

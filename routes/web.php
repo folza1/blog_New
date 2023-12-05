7aa9f9228d8161c4850255f08222c686df531866
folza <fodorzoltan317@gmail.com>
2023-12-05 20:46:01 +0100

    26.Clockwork,_And_The_N+1_Problem

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
     return view('posts',[
-        'posts'=>Post::all()
+        'posts'=>Post::with('category')->get()
     ]);
 });
 
@@ -30,7 +30,7 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 
 });
 
-Route::get('categories/{category:slug}', function (Category $category) {
+Route::get('/categories/{category:slug}', function (Category $category) {
 
     return view('posts', [
         'posts' => $category->posts

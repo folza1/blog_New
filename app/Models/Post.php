8e8d6b8a18d3af7d3508971986a30d6c242ee3f1
folza <fodorzoltan317@gmail.com>
2023-12-05 17:51:12 +0100

    23.Route_Model_Binding

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,5 +9,10 @@ class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = [];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }

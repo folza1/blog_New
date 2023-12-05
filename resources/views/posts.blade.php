3cee6427e6f3f54bc17a6a369c52d61332e4d942
folza <fodorzoltan317@gmail.com>
2023-12-05 19:08:13 +0100

    24.Your_First_Eloquent_Relationship

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,6 +5,11 @@
                 <a href="/posts/{{$post->slug}}">
                     {!!$post->title!!}        </a>
             </h1>
+
+            <p>
+            <a href="#">{{$post->category->name}}</a>
+            </p>
+
             <div>
                 {!!$post->excerpt!!}
             </div>

de3c2ff023c51fd1580aab862c03819321f0173e
folza <fodorzoltan317@gmail.com>
2023-12-06 12:57:36 +0100

    27.Database_Seeding_Saves_Time

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,51 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal',
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family',
+        ]);
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work',
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $personal->id,
+            'title' => 'Personal Post',
+            'slug' => 'personal-post',
+            'excerpt' => '<p>This is my personal post</p>',
+            'body' => '<p>This is my personal post bodyis my personal post bodyis my personal post bodyis my personal post bodyis my personal post bodyis my personal post bodyis my personal post bodyis my personal post bodyis my personal post bodyis my personal post bodyis my personal post bodyis my personal post bodyis my personal post body</p>',
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'Family Post',
+            'slug' => 'family-post',
+            'excerpt' => '<p>This is my family post</p>',
+            'body' => '<p>This is my family post body is my family post body is my family post body is my family post body is my family post body is my family post body is my family post body is my family post body is my family post body is my family post body is my family post body is my family post body is my family post body is my family post body</p>',
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'Work Post',
+            'slug' => 'work-post',
+            'excerpt' => '<p>This is my work post</p>',
+            'body' => '<p>This is a work post body is a work post body is a work post body is a work post body is a work post body is a work post body is a work post body is a work post body is a work post body is a work post body is a work post body is a work post body is a work post body is a work post body is a work post body is a work post body</p>',
+        ]);
     }
 }

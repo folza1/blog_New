4e41ad709da2b48c7a61317982a5d375ca1fe626
folza <fodorzoltan317@gmail.com>
2023-12-07 20:20:10 +0100

    34.A_Small_Javascript_Dropdown_Detour

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,7 @@
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/2.8.2/alpine-ie11.js" integrity="sha512-KA2IvqHkHQMVbJlMnypXkjiHFkYAVaYSd193ckoVZYZ7AH2qZqlv7odiJcjAF2lECSEHptHFrrKaIWCXqZCXHg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <body style="font-family: Open Sans, sans-serif">
 <section class="px-6 py-8">

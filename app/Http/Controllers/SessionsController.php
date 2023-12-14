bde8cc09ae54b4591d1d654f1fd7a7d0bfba138a
folza <fodorzoltan317@gmail.com>
2023-12-14 16:16:57 +0100

    50.Build_The_Log_In_Page

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,9 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class SessionsController extends Controller
 {
+    public function create(){
+        return view('sessions.create');
+    }
+
+    public function store(){
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if(auth()->attempt($attributes)){
+            session()->regenerate();
+            return redirect('/')->with('success', 'Welcome back!');
+        }
+
+        throw ValidationException::withMessages(['email' => 'Your provided credentials could not be verified.']);
+
+    }
+
     public function destroy()
     {
         auth()->logout();

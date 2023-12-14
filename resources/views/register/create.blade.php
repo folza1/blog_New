df5a77778a06947016e1bb67b3eb921329e5195c
folza <fodorzoltan317@gmail.com>
2023-12-14 13:23:46 +0100

    47.Failed_Validation_And_Old_Input_Data

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -13,8 +13,12 @@
                         type="text"
                         name="name"
                         id="name"
+                        value="{{old('name')}}"
                         required
                     />
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -26,8 +30,12 @@
                         type="text"
                         name="username"
                         id="username"
+                        value="{{old('username')}}"
                         required
                     />
+                    @error('username')
+                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -39,8 +47,12 @@
                         type="email"
                         name="email"
                         id="email"
+                        value="{{old('email')}}"
                         required
                     />
+                    @error('email')
+                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -54,6 +66,9 @@
                         id="password"
                         required
                     />
+                    @error('password')
+                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -63,6 +78,7 @@
                         Submit
                     </button>
                 </div>
+
             </form>
         </main>
     </section>
